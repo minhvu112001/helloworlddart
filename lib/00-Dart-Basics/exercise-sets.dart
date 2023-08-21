695bfaa7fb1ddcaabbc6ba9b81a62b7b7600f62b
minhvu112001 <81789695+minhvu112001@users.noreply.github.com>
2023-08-21 17:02:43 +0700

    all

---
+++ lib/00-Dart-Basics/exercise-sets.dart
@@ -0,0 +1,6 @@
+void main(){
+  var footballTeam = {'Andrew', 'Zach', 'Calvin'};
+  var playCast = {'David', 'Calvin', 'Claire'};
+  print(footballTeam.intersection(playCast));
+  print(playCast.intersection(footballTeam));
+}
---
+++ lib/00-Dart-Basics/sets.dart
@@ -0,0 +1,8 @@
+void main(){
+  var elements = {'O', 'K', 'Ni', 'Rd'};
+  elements.add('F');
+  elements.addAll(['H','C']);
+  print(elements);
+  elements.add('C');
+  print(elements);
+}
