02e2324b1229975d866991f5bbc1c25aaba8233d
minhvu112001 <81789695+minhvu112001@users.noreply.github.com>
2023-08-23 13:56:56 +0700

    all

---
+++ lib/slide3/codesnipper3.dart
@@ -0,0 +1,4 @@
+int num=5;
+if (num>0){
+  print('Number is positive!');
+}
---
+++ lib/slide3/codesnippet2.dart
@@ -0,0 +1,10 @@
+void main(){
+  int a=7;
+  int b=5;
+  bool c=a > 10 && b < 10;
+  print(c);
+  bool d=a > 10 || b < 10;
+  print(d);
+  bool e=! (a > 10) ;
+  print(e);
+}
