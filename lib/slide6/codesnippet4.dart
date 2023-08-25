eac742c5b38c54d8c32c8605e2ab7f0f6e5ec790
minhvu112001 <81789695+minhvu112001@users.noreply.github.com>
2023-08-25 15:33:28 +0700

    all

---
+++ lib/slide6/codesnippet4.dart
@@ -0,0 +1,7 @@
+import 'dart:async';
+void main() {
+Future<int> getFuture(){
+  return Future.error("This is an error");
+}
+  getFuture();
+}
---
+++ lib/slide6/codesnippet5.dart
@@ -0,0 +1,6 @@
+import 'dart:async';
+void main(){
+  Future.delayed(Duration(milliseconds:10000),(){
+    print("This is a delayed future");
+  });
+}
