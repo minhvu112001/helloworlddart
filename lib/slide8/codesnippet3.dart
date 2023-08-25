13750c82ae6a9456fd6bda9527b9ac0124864d84
minhvu112001 <81789695+minhvu112001@users.noreply.github.com>
2023-08-25 21:01:26 +0700

    all

--- lib/slide8/codesnippet3.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:convert';
-main(){
-  final json = '{"productName":"Suit", "brand": "Armani}';
-  final parsedJson = jsonDecode(json);
-  print('${parsedJson.runtimeType}: $parsedJson');
-}
--- lib/slide8/main.dart
+++
@@ -1,5 +0,0 @@
-final json = '{"productName":  "Suit",
-    "brand": "Armani",  "reviews": [{"rating":
-4.5,"review":  "Superb brand!"},{"rating":
-5.0,"review": "Amazing fabric and look!"}]}';
-
